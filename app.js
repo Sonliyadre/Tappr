1adaef9153028f94aa48cf9db85f7fd2b10b855d
Nyan of the Moon <nyanofthemoon@gmail.com>
2016-03-11 18:46:43 +0000

    moved event names to config file

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var options = {
 
 var gameId              = null;
 var leaderboardInterval = null;
-var game_status         = 'stopped';
+var game_status         = CONFIG.game.status.STOPPED;
 
 var players         = [];
 var admin_socket_id = null;
@@ -72,7 +72,7 @@ io.on('connection', function(socket) {
 
     });
     
-    socket.on('add_player', function(data) {
+    socket.on(CONFIG.game.event.PLAYER_ADD, function(data) {
         var isAlreadyInList = false;
         for (var index in players) {
             if (players[index].name == data.name) {
@@ -83,57 +83,54 @@ io.on('connection', function(socket) {
         
         if (isAlreadyInList) {
              console.log(data.name + " already exists");
-             socket.emit('add_player', {'addition': false, 'message': 'Already in list'});
+             socket.emit(CONFIG.game.event.PLAYER_ADD, {'addition': false, 'message': 'Already in list'});
         } else {
             console.log(data.name + " was added");
             players.push({name: data.name, socket_id: this.id, tap_count:0});
             socket.join(gameId);
-            socket.emit('add_player', {'addition': true, 'message': 'Welcome!'});
+            socket.emit(CONFIG.game.event.PLAYER_ADD, {'addition': true, 'message': 'Welcome!'});
         }
     });
     
     socket.on('timer_start', function(data) {
-        if (admin_socket_id === this.id && game_status === 'stopped') {
+        if (admin_socket_id === this.id && game_status === CONFIG.game.status.STOPPED) {
             players     = [];
-            game_status = 'waiting';
+            game_status = CONFIG.game.status.WAITING;
             gameId      = generateUniqueGameId();
                 
             console.log('Starting New Game with Channel ID ' + gameId);
             setTimeout(function() {
-                game_status = 'started';
-                io.to(admin_socket_id).emit('game_start');
-                io.to(gameId).emit('game_start');
+                game_status = CONFIG.game.status.STARTED;
+                io.to(admin_socket_id).emit(CONFIG.game.event.GAME_START);
+                io.to(gameId).emit(CONFIG.game.event.GAME_START);
                 leaderboardInterval = setInterval(function() {
-                    io.to(admin_socket_id).emit('tap_update', sanitizePlayerData());
+                    io.to(admin_socket_id).emit(CONFIG.game.event.PLAYER_DATA, sanitizePlayerData());
                 }, CONFIG.game.intervalLeaderboard);
-                
-                //******************************************************
-                //@NOTE yes, this was done just for testing purposes - we will put it back to 300000 when we are ready to test fully
             }, CONFIG.game.intervalTimer);
         }
     
     });
     
-    socket.on('tap_update', function(data) {
-        socket.emit('tap_update', sanitizePlayerData());
+    socket.on(CONFIG.game.event.PLAYER_DATA, function(data) {
+        socket.emit(CONFIG.game.event.PLAYER_DATA, sanitizePlayerData());
     });
 
-    socket.on('game_status', function(data) {
-        socket.emit('game_status', {status: game_status});
+    socket.on(CONFIG.game.event.GAME_STATUS, function(data) {
+        socket.emit(CONFIG.game.event.GAME_STATUS, {status: game_status});
     });
     
-    socket.on('player_click', function(data) {
-        if (game_status != 'stopped') {
+    socket.on(CONFIG.game.event.PLAYER_CLICK, function(data) {
+        if (game_status != CONFIG.game.status.STOPPED) {
             for (var index in players) {
                 if (players[index].socket_id == this.id) {
                     players[index].tap_count++;
                     console.log('Incremented tap count for ' + players[index].name);
                     if (players[index].tap_count >= CONFIG.game.maxTap) {
                         console.log('Ending Existing Game with Channel ID ' + gameId);
-                        game_status = 'stopped';
+                        game_status = CONFIG.game.status.STOPPED;
                         clearInterval(leaderboardInterval);
-                        io.to(admin_socket_id).emit('game_stop');
-                        socket.to(gameId).emit('game_stop');
+                        io.to(admin_socket_id).emit(CONFIG.game.event.GAME_STOP);
+                        socket.to(gameId).emit(CONFIG.game.event.GAME_STOP);
                     }
                     
                     break;
