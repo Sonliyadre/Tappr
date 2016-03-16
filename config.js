6f3ac7bca49c7dbd892b2f1a694b79b66c295623
Nyan of the Moon <nyanofthemoon@gmail.com>
2016-03-16 18:37:42 +0000

    added environment variables

--- config.js
+++ config.js
@@ -9,14 +9,14 @@ module.exports = {
     },
 
     game: {
-        maxTap:              process.env.MAX_TAP              || 300,
-        intervalLeaderboard: process.env.INTERVAL_LEADERBOARD || 333,
-        intervalTimer:       process.env.INTERVAL_TIMER       || 10000,
-        effectMinimumTime:  2500,
-        effectAdditionTime: 2500,
-        effectLastingChance: 50,
-        effectInstantChance: 75,
-        minimumPlayers     : 2,
+        maxTap:              process.env.MAX_TAP               || 300,
+        intervalLeaderboard: process.env.INTERVAL_LEADERBOARD  || 333,
+        intervalTimer:       process.env.INTERVAL_TIMER        || 10000,
+        effectMinimumTime:   process.env.EFFECT_MINIMUM_TIME   || 2500,
+        effectAdditionTime:  process.env.EFFECT_ADDITION_TIME  || 2500,
+        effectLastingChance: process.env.EFFECT_CHANCE_LASTING || 50,
+        effectInstantChance: process.env.EFFECT_CHANCE_INSTANT || 75,
+        minimumPlayers     : process.env.MINIMUM_PLAYERS || 1,
 
         status: {
             STARTED: 'started',
